0a1fa8b975ec15c9993dd7206da62049589a9396
ringo-neko <shunsukesato1106@gmail.com>
2025-06-17 16:54:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,16 @@ window.onload = function() {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   canvas.addEventListener('mousedown', (e) => {(touched=e)});
-  for(i=0;i<10000;i++) {
-    if(i>w || i*1.5>h) {
-      canvas.width=i-1;
-      canvas.height=i*1.5-1.5;
-      break;
-    }   
+
+  while(true) {
+    for(i=0;i<10000;i++) {
+      if(i>w || i*1.5>h) {
+        canvas.width=i-1;
+        canvas.height=i*1.5-1.5;
+        break;
+      }   
+    }
+    ctx.fillStyle="rgb(0,0,0)"
+    ctx.fillRect(0,0,canvas.width,canvas.height)
   }
 }
